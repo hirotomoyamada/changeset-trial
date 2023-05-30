fbb53aa2eb77e9104c5f1ab7cb9f613114824907
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-05-30 10:02:15 +0900

    update

--- packages/a/index.ts
+++ packages/a/index.ts
@@ -17,3 +17,4 @@ console.log('updated @changeset-trial/a');
 console.log('updated @changeset-trial/a');
 console.log('updated @changeset-trial/a');
 console.log('updated @changeset-trial/a');
+console.log('updated @changeset-trial/a');
